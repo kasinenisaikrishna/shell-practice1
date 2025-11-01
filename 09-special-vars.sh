2fd280db0fc0b7b5e9fd7fb77f66d9f6a44de171
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-01 15:27:59 +0530

    shell

---
+++ 09-special-vars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "all variables passed to the script: $@"
+echo "no.of vars/args passed to the script: $#"
+echo "script name: $0"
+#while running sh 09-special-vars.sh sai 23 97
+echo "cwd: $PWD"
+echo "home dir of current user: $HOME"
