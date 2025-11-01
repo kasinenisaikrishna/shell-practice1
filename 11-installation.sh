a7ed77e9c69171366a156aa3d75528c6ca8ac7c9
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-01 19:07:54 +0530

    shell

--- 11-installation.sh
+++ 11-installation.sh
@@ -5,5 +5,13 @@ then
     echo "please run this script with root access" #this script should exit after this line but this will not hence we should place exit status
     exit 1
 fi
-dnf install git -y
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    echo "git is not installed, going to install it.."
+    dnf install git -y
+else
+    echo "git is already installed, nothing to do.."
+fi
+    
 
