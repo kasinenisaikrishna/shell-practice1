189813e94cfe75c25cd8bfab92be37eb90df3c00
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-01 11:57:46 +0530

    shell

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh:: hi suresh, how are you?"
+echo "suresh:: hello ramesh. I am fine how are you doing?"
+echo "ramesh:: i am doing good suresh. What is going on?"
+echo "suresh:: i started learning shell script ramesh"
