b358d71a4e83b78c60af8e8176db899e83dfa308
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-01 12:37:23 +0530

    shell

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+person1=ramesh
+person2=suresh
+echo "$person1:: hi $person2, how are you?"
+echo "$person2:: hello $person1. I am fine how are you doing?"
+echo "$person1:: i am doing good $person2. What is going on?"
+echo "$person2:: i started learning shell script $person1"
