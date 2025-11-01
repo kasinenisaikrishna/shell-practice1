86e9f9fe1506e9c87bb8b8f3a90f72beffdc0d01
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-01 19:11:23 +0530

    shell

--- 11-installation.sh
+++ 11-installation.sh
@@ -3,9 +3,9 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then
     echo "please run this script with root access" #this script should exit after this line but this will not hence we should place exit status
-    exit 1
+    exit 1 #placing this to break from script
 fi
-dnf list installed git
+dnf list installed git #comamnd to know whether git is installed or not?
 if [ $? -ne 0 ]
 then
     echo "git is not installed, going to install it.."
