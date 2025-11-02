d384bfe4a320d3c593a7a6847aded4cd4ec6b7e7
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-02 07:45:08 +0530

    shell

--- 12.functions.sh
+++ 12.functions.sh
@@ -1,48 +1,39 @@
 #!/bin/bash
 userid=$(id -u)
-validate(){
+validate(){ #validate function body
     if [ $1 -ne 0 ]
     then
-        echo "Command is...FAILED"
+        echo "$2 is...FAILED"
         exit 1
     else
-        echo "Command is...SUCCESS"
+        echo "$2 is...SUCCESS"
     fi 
 }
+
 if [ $userid -ne 0 ]
 then
     echo "please run this script with root access" 
     exit 1 
 fi
+
 dnf list installed git 
-validate $?
+
 if [ $? -ne 0 ] 
 then
     echo "git is not installed, going to install it.."
     dnf install git -y
-    if [ $? -ne 0 ]
-    then
-        echo "git installation is not success...check it"
-        exit 1
-    else
-        echo "git installation is successful"
-    fi
+    validate $? "installing git" #calling the validate function
 else
     echo "git is already installed, nothing to do.."
 fi
 
 dnf list installed mysql
+
 if [ $? -ne 0 ] 
 then
     echo "mysql is not installed... going to install"
     dnf install mysql -y
-    if [ $? -ne 0 ]
-    then
-        echo "mysql installation is not success...check it"
-        exit 1
-    else
-        echo "mysql installation is successful"
-    fi
+    validate $? "installing mysql"
 else
     echo "mysql is already installed... nothing to do"
 fi
