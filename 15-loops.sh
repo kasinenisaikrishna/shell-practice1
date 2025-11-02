1ff6863ce53e11f35a2a4a6dcc3b4501cfd1a9e1
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-02 11:58:04 +0530

    shell

---
+++ 15-loops.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+userid=$(id -u)
+r="\e[31m"
+g="\e[32m]"
+y="\e[33m"
+n="\e[0m]"
+
+check_root(){
+    if [ $userid -ne 0 ]
+    then
+        echo "please run this script with root access" 
+        exit 1 
+    fi
+}
+
+validate(){ 
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$r FAILED $n"
+        exit 1
+    else
+        echo -e "$2 is...$g SUCCESS $n"
+    fi 
+}
+
+check_root
+
+#sh 15-loops.sh git mysql postfix nginx
+for package in $@ #$@ is a special variables refers to all arguments passed to it
+do
+    dnf list installed $package
+    if [ $? -ne 0 ]
+    then
+        echo "$package is not installed, going to install it.."
+        dnf install $package -y
+        validate $? "installing $package"
+    else
+        echo "$package is already installed.. nothing to do"
+    fi
+done
