443ffd5360b935922b382ea3e07e5d0cbdb8c243
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-11-03 06:08:35 +0530

    expense

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 set -e #setting up automatic exit if we get an error, set -ex for debug mode
+
+failure(){
+    echo "failed at: $1:$2"
+}
+
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR #ERR is the error signal
+
 echo "hi from success"
 echoooo "hi from failure"
 echo "hi after failure"
